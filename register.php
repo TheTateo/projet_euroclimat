4780be674d42c6cec568612fa803ee0c096c8bd8
tboyer3 <tboyer3@LLC-C306-03>
2026-02-11 17:09:28 +0100

    Ajout CSS register.php

--- register.php
+++ register.php
@@ -18,19 +18,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 
-<h2>Créer un compte</h2>
-<form method="POST">
-    <label>Nom d'utilisateur :</label>
-    <input type="text" name="username" required><br>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div>
+        <h2>Créer un compte</h2>
+        <form method="POST">
+            <label>Nom d'utilisateur :</label>
+            <input type="text" name="username" required><br>
 
-    <label>Email :</label>
-    <input type="email" name="email" required><br>
+            <label>Email :</label>
+            <input type="email" name="email" required><br>
 
-    <label>Mot de passe :</label>
-    <input type="password" name="mot_de_passe" required><br>
+            <label>Mot de passe :</label>
+            <input type="password" name="mot_de_passe" required><br>
 
-    <button type="submit">Envoyer la demande</button>
-</form>
-
+            <button type="submit">Envoyer la demande</button>
+        </form>
+        <a href="index.html">Retour</a>
+    </div>
+</body>
 <br>
-<a href="index.html">Retour</a>
